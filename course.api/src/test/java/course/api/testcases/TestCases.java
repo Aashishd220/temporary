98c06cfefcc4859bc6379a3497844566ae6f7205
Aashishd220 <41561579+Aashishd220@users.noreply.github.com>
2018-08-23 18:11:29 +0530

    Add files via upload

---
+++ course.api/src/test/java/course/api/testcases/TestCases.java
@@ -0,0 +1,14 @@
+package course.api.testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCases {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
